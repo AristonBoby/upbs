9af83c203cfcd8b77b7372f71d7efcef3e5b2b77
Aristonbobi <aristonbobi@gmail.com>
2024-08-07 15:33:40 +0800

    update

--- app/Livewire/Profil/Password.php
+++ app/Livewire/Profil/Password.php
@@ -2,12 +2,35 @@
 
 namespace App\Livewire\Profil;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Livewire\Component;
 
 class Password extends Component
 {
+    public $passLama;
+    public $passBaru;
+    public $repassBaru;
+
     public function render()
     {
         return view('livewire.profil.password');
+    }
+
+    public $rules = [
+        'passLama'      => 'required',
+        'passBaru'      => 'required',
+        'repassBaru'    => 'required|same:passBaru',
+    ];
+
+    public function passwordUpdate()
+    {
+        $this->validate();
+        $id = Auth::user()->id;
+        $query = User::find($id)->update([
+            'password' => Hash::make($this->passBaru),
+
+        ]);
     }
 }
--- resources/views/livewire/profil/password.blade.php
+++ resources/views/livewire/profil/password.blade.php
@@ -9,23 +9,26 @@
                 <div class="form-group row mb-2">
                     <label class="col-form-label col-md-4">Password Lama <span class="text-danger">*</span></label>
                     <div class="col-md-8">
-                        <input type="text" class="form-control form-control-sm rounded-0">
+                        <input type="text" wire:model='passLama' class="form-control form-control-sm rounded-0 @error($passLama) is-invalid @enderror">
+                        @error($passLama) <div class="invalid-feedback">{{ $message }}</div> @enderror
                     </div>
                 </div>
                 <div class="form-group row mb-2">
                     <label class="col-form-label col-md-4">Password Baru <span class="text-danger">*</span></label>
                     <div class="col-md-8">
-                        <input type="text" class="form-control form-control-sm rounded-0">
+                        <input type="text" wire:model='passBaru' class="form-control form-control-sm rounded-0 @error($passBaru) is-invalid @enderror">
+                        @error($passBaru) <div class="invalid-feedback">{{ $message }}</div> @enderror
                     </div>
                 </div>
                 <div class="form-group row mb-2">
-                    <label class="col-form-label col-md-4">Re-Password Baru <span class="text-danger">*</span></label>
+                    <label class="col-form-label col-md-4" >Re-Password Baru <span class="text-danger">*</span></label>
                     <div class="col-md-8">
-                        <input type="text" class="form-control form-control-sm rounded-0">
+                        <input type="text" wire:model='repassBaru' class="form-control form-control-sm rounded-0 @error($repassBaru) is-invalid @enderror">
+                        @error($repassBaru) <div class="invalid-feedback">{{ $message }}</div> @enderror
                     </div>
                 </div>
                 <div class="mt-3">
-                    <button type="submit" data-bs-dismiss="modal" class="col-md-2 float-end mx-1 btn btn-sm btn-danger"><i class="fa fa-times"></i> Batal</button>
+                    <button type="button" data-bs-dismiss="modal" class="col-md-2 float-end mx-1 btn btn-sm btn-danger"><i class="fa fa-times"></i> Batal</button>
                 </div>
                 <div class="mt-3">
                     <button type="submit" class="col-md-2 float-end btn btn-sm btn-primary" ><i class="fa fa-save"></i> Simpan</button>

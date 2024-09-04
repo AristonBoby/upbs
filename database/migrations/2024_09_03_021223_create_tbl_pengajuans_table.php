56184c44705401890d53a935f63e8030cfa3c1eb
Ariston Boby <aristonbobi@gmail.com>
2024-09-04 15:23:17 +0800

    update

--- database/migrations/2024_09_03_021223_create_tbl_pengajuans_table.php
+++ database/migrations/2024_09_03_021223_create_tbl_pengajuans_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\jenisPembayaran;
 use App\Models\kelurahan;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -17,10 +18,12 @@ return new class extends Migration
             $table->uuid('id')->primary();
             $table->foreignIdFor(user::class);
             $table->foreignIdFor(kelurahan::class);
+            $table->foreignIdFor(jenispembayaran::class);
             $table->char('status',2);
             $table->softDeletes();
             $table->timestamps();
             $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('jenispembayaran_id')->references('id')->on('jenispembayarans');
             $table->foreign('kelurahan_id')->references('id')->on('kelurahans');
 
         });
